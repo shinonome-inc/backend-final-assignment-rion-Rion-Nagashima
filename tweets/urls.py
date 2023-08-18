af3b8e6ddab8cab3b4477d89ebfdc3af9404b6d5
enji-engineer0204 <enji-engineer0204@gmail.com>
2023-08-18 21:27:01 +0900

    Implemented signup feature and fixed URL routing issue

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,11 +1,11 @@
-# from django.urls import path
+from django.urls import path
 
-# from . import views
+from .views import home
 
 app_name = "tweets"
 
 urlpatterns = [
-    # path('home/', views.HomeView.as_view(), name='home'),
+    path("home/", home, name="home"),
     # path('create/', views.TweetCreateView.as_view(), name='create'),
     # path('<int:pk>/', views.TweetDetailView.as_view(), name='detail'),
     # path('<int:pk>/delete/', views.TweetDeleteView.as_view(), name='delete'),
--- tweets/views.py
+++ tweets/views.py
@@ -1,1 +1,5 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, "tweets/home.html")
